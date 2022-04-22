2b32e630c6d03228094a7b83d45795e65b711405
Natali-Kremneva <van2gog6@gmail.com>
2022-04-23 00:23:32 +0300

    Initial commit

---
+++ src/main/java/com/example/orderprojectdemo/OrderProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProjectDemoApplication.class, args);
+    }
+
+}
